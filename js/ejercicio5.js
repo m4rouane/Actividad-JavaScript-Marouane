7c74830483088a85e939300b8829a925c04d0bdc
Marouane Saidi Rahim <marsairah@alu.edu.gva.es>
2023-11-15 17:33:12 +0100

    Commit - Ejercicio 5 resuelto MSR

--- js/ejercicio5.js
+++ js/ejercicio5.js
@@ -1,5 +1,5 @@
 /**
- * @author: Escriba su nombre y apellidos aquí.
+ * @author: Marouane Saidi Rahim
  */
 
 /**
@@ -8,30 +8,55 @@
  * @returns true | false;
  */
 
-function esMayorEdad(edad)
-{
-    return 0; // Debe escribir el código correcto AQUÍ.
+function esMayorEdad(edad) {
+    if (edad >= 18) {
+        return true;
+    } else {
+        return false;
+    }
+     // Debe escribir el código correcto AQUÍ.
 }
 /**
  * Obtiene la calificación equivalente a la nota numérica
  * @param {int} nota: entero. 
  * @returns  cadena: Calificación
  */
-function obtenerCalificacion(nota)
-{
-    var calificacion="Incorrecta";
+function obtenerCalificacion(nota) {
+    var calificacion="";
 
             // Estructura de control *** switch(nota) ***
-            
+            switch (true) {
+                case nota >= 0 && nota < 3:
+                    calificacion = "Muy deficiente";
+                    break;
+                case nota >= 3 && nota < 5:
+                    calificacion = "Insuficiente";
+                    break;
+                case nota >= 5 && nota < 6:
+                    calificacion = "Suficiente";
+                    break;
+                case nota >= 6 && nota < 7:
+                    calificacion = "Bien";
+                    break;
+                case nota >= 7 && nota < 9:
+                    calificacion = "Notable";
+                    break;
+                case nota >= 9 && nota <= 10:
+                    calificacion = "Sobresaliente";
+                    break;
+                default:
+                    calificacion = "Incorrecta";
+                    break;
             // Complete el codigo AQUÍ
 
-            return calificacion;
+            
+}
+return calificacion;
 }
 /**
  * Comprueba la mayoria de edad en la página HTML.
  */
-function comprobarEdad()
-{
+function comprobarEdad() {
     var edad=Number.parseInt(document.getElementById("edad").value);
     console.log('Edad: '+edad);
     console.log('Mayoría de edad?: '+esMayorEdad(edad));
@@ -46,8 +71,7 @@ function comprobarEdad()
 /**
  * Escribe la calificación asociada a la nota en la página HTML.
  */
-function averiguarCalificacion()
-{
+function averiguarCalificacion() {
     var nota=Number.parseInt(document.getElementById("nota").value);
     console.log('Nota: '+nota);
     console.log('Calificación: '+obtenerCalificacion(nota));
